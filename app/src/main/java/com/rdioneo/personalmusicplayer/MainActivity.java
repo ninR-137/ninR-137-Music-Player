a726b75d3dce4790a6793a3348348544a5617977
ninR-137 <ninpspod@gmail.com>
2022-07-25 13:03:42 +0800

    Added working storage permission request

--- app/src/main/java/com/rdioneo/personalmusicplayer/MainActivity.java
+++ app/src/main/java/com/rdioneo/personalmusicplayer/MainActivity.java
@@ -1,9 +1,18 @@
 package com.rdioneo.personalmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
@@ -49,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     // Stoping the music
     public void musicstop()
     {
@@ -57,4 +65,34 @@ public class MainActivity extends AppCompatActivity {
         music = MediaPlayer.create(this, R.raw.live_another_day);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //YOU BETTER ACCEPT THIS  SHIT OR ITS NOT GOING ANYWHERE
+
+        //Permission for SDK between 23 and 29
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        100);
+            }
+        }
+        //Permission for SDK 30 and above
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            if(!Environment.isExternalStorageManager())
+                try{
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    intent.setData(Uri.parse(String.format("package:%s", getApplicationContext().getPackageName())));
+                    startActivityIfNeeded(intent, 101);
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    startActivityIfNeeded(intent, 101);
+                }
+        }
+    }
 }
